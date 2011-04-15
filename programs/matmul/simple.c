911b59b38fa422eef98a1f39f64e05aa7f51047d
Raphael 'kena' Poss <r.c.poss@uva.nl>
2011-04-15 18:27:17 +0200

    [sl-programs] Improve the `matmul' benchmark.
    * matmul/simple.c: Ensure that each multiply phase has an output
    matrix distinct from the input.
    
    * matmul/Makefile.am (.c.tar): Add a rule to generate Unibench
    archives.
    
    * matmul/input.d10,
    * matmul/input.d30: Define less iterations and sane values to
    avoid overflows.

--- programs/matmul/simple.c
+++ programs/matmul/simple.c
@@ -1,9 +1,25 @@
 #include <svp/fibre.h>
 #include <svp/perf.h>
+#include <svp/testoutput.h>
 #include <assert.h>
-#include <stdio.h>
+
 
 #include "matmul.c"
+
+/*
+void printm(size_t n, number (*m)[n][n])
+{
+    for (size_t j = 0; j < n; ++j)
+    {
+        for (size_t i = 0; i < n; ++i)
+        {
+            output_int((long)((*m)[j][i]), 1);
+            output_char(' ',1);
+        }
+        output_char('\n', 1);
+    }         
+}
+*/
 
 sl_def(t_main, void)
 {
@@ -11,14 +27,11 @@ sl_def(t_main, void)
 
     size_t L = 1;
 
-    if (fibre_rank(1) == 0)
-    {
-        assert(fibre_tag(0) == 0);
-        assert(fibre_rank(0) == 0);
-        L = *(unsigned long*)fibre_data(0);
-        ++f;
-    }
+    assert(fibre_tag(f) == 0);
+    assert(fibre_rank(f) == 0);
+    L = *(unsigned long*)fibre_data(f);
 
+    ++f;
     assert(fibre_tag(f) == 0);
     assert(fibre_rank(f) == 0);
     size_t input_n = *(unsigned long*)fibre_data(f);
@@ -32,41 +45,65 @@ sl_def(t_main, void)
     assert(fibre_rank(f) == 2);
     assert(fibre_shape(f)[0] == N);
     assert(fibre_shape(f)[1] == N);
-    ++f;
-    assert(fibre_rank(f) == 2);
-    assert(fibre_shape(f)[0] == N);
-    assert(fibre_shape(f)[1] == N);
-    ++f;
-    assert(fibre_rank(f) == 2);
-    assert(fibre_shape(f)[0] == N);
-    assert(fibre_shape(f)[1] == N);
-    ++f;
-    assert(fibre_rank(f) == 2);
-    assert(fibre_shape(f)[0] == N);
-    assert(fibre_shape(f)[1] == N);
+    number (*a)[N][N] = (number (*)[N][N])fibre_data(f);
 
-    number (*a)[N][N] = (number (*)[N][N])fibre_data(2);
-    number (*b)[N][N] = (number (*)[N][N])fibre_data(3);
-    number (*expected)[N][N] = (number (*)[N][N])fibre_data(4);
-    number (*c)[N][N] = (number (*)[N][N])fibre_data(5);
+    ++f;
+    assert(fibre_rank(f) == 2);
+    assert(fibre_shape(f)[0] == N);
+    assert(fibre_shape(f)[1] == N);
+    number (*b)[N][N] = (number (*)[N][N])fibre_data(f);
+
+    ++f;
+    assert(fibre_rank(f) == 2);
+    assert(fibre_shape(f)[0] == N);
+    assert(fibre_shape(f)[1] == N);
+    number (*expected)[N][N] = (number (*)[N][N])fibre_data(f);
+
+    ++f;
+    assert(fibre_rank(f) == 2);
+    assert(fibre_shape(f)[0] == N);
+    assert(fibre_shape(f)[1] == N);
+    number (*c)[N][N] = (number (*)[N][N])fibre_data(f);
+
+    number (*c_other)[N][N] = malloc(sizeof(number)*N*N);
+    assert(c_other != 0);
+    number (*tmp)[N][N];
 
 
     struct s_interval ivs[L + 1];
     size_t i, j;
 
     mtperf_start_interval(ivs, 0, 0, "work");
-    for (i = 1; i <= L; ++i)
+    mtperf_start_interval(ivs, 1, 1, "compute");
+    sl_proccall(matmul,
+                sl_glarg(void*, , a),
+                sl_glarg(void*, , b),
+                sl_glarg(void*, , c)
+#ifndef N
+                , sl_glarg(size_t, , N)
+#endif
+        );
+    mtperf_finish_interval(ivs, 1);
+
+    // printm(N, c); output_string("----\n", 1);
+    
+    for (i = 2; i <= L; ++i)
     {
         mtperf_start_interval(ivs, i, i, "compute");
         sl_proccall(matmul,
                     sl_glarg(void*, , a),
-                    sl_glarg(void*, , b),
-                    sl_glarg(void*, , c)
+                    sl_glarg(void*, , c),
+                    sl_glarg(void*, , c_other)
 #ifndef N
-                    ,       sl_glarg(size_t, , N)
+                    , sl_glarg(size_t, , N)
 #endif
             );
         mtperf_finish_interval(ivs, i);
+        tmp = c;
+        c = c_other;
+        c_other = tmp;
+
+        // printm(N, c); output_string("----\n", 1);
     }
     mtperf_finish_interval(ivs, 0);
     mtperf_report_intervals(ivs, L, REPORT_FIBRE|REPORT_STREAM(2));
@@ -79,6 +116,7 @@ sl_def(t_main, void)
             if (v < 0) v = -v;
             if (v > max) max = v;
         }
-    printf("%ld\n", ((long)(max * 1000)));
+    output_int(((long)(max * 1000)), 1);
+    output_char('\n', 1);
 }
 sl_enddef
