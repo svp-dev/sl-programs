297e86466a37e5abbbce7ead505389301ce1a8b9
Raphael 'kena' Poss <r.c.poss@uva.nl>
2012-05-29 12:50:55 +0200

    [sl-programs] Fix the checkers demo.
    * demo/checkers.c: Fix the loop ierations to fit in the configured
    resolutions (10x10).

--- programs/demo/checkers.c
+++ programs/demo/checkers.c
@@ -20,8 +20,8 @@ sl_def(t_main, void)
   gfx_resize(10, 10);
 
   unsigned long x, y;
-  for (y = 0; y < 100; ++y)
-      for (x = 0; x < 100; ++x)
+  for (y = 0; y < 10; ++y)
+      for (x = 0; x < 10; ++x)
           gfx_putpixel(x, y, (x + y)%2 ? 0xffffff : 0);
 
   gfx_dump(0, 1, 0, 0);
